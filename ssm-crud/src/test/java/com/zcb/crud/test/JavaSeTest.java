5c430a0cc172120a9213b73df26f2d0273810470
icecfish <90397150+icecfish@users.noreply.github.com>
2021-09-10 09:38:59 +0800

    Update JavaSeTest.java

--- ssm-crud/src/test/java/com/zcb/crud/test/JavaSeTest.java
+++ ssm-crud/src/test/java/com/zcb/crud/test/JavaSeTest.java
@@ -14,5 +14,6 @@ public class JavaSeTest {
         System.out.println("hello master 3th！");
         System.out.println("hello git-fix-hot 2th");
         System.out.println("test push");
+        System.out.println("不会有人再githhub上面直接改代码吧。不会吧");
     }
 }
