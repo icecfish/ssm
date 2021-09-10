7b11d8d78aedcf7126d8bab88070f686c5ce5c72
zcb <zcb@666.com>
2021-09-10 08:58:34 +0800

    ssm整合第二版

--- ssm-crud/src/test/java/com/zcb/crud/test/JavaSeTest.java
+++ ssm-crud/src/test/java/com/zcb/crud/test/JavaSeTest.java
@@ -9,11 +9,6 @@ import org.junit.Test;
 public class JavaSeTest {
     @Test
     public void test(){
-        String s = "abc";
-        String s2 = "abc";
-        String s1 = new String("abc");
-        s1=s2;
-        System.out.println(s1==s);
-
+        System.out.println("just test git");
     }
 }
